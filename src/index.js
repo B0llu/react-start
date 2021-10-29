e94ccb2e77ed0593dce09ee15e7f8280a2505a19
Dhruv <samantdhruv@gmail.com>
2021-10-29 09:13:12 +0530

    Revert "first component"
    This reverts commit caecdd9473ba5edc3964c07c2bf2a6dc207acdd2.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-
-function greeting() {
-  return <h1> This is bollu and this is my first component </h1>;
-}
-
-ReactDom.render(greeting(), document.getElementById("root"));
