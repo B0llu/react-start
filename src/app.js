c2bfda31550847067053518549e4f650dbbbffcb
Dhruv <samantdhruv@gmail.com>
2021-10-30 10:30:59 +0530

    word meaning

--- src/app.js
+++ src/app.js
@@ -1,17 +1,53 @@
 import React, { useState } from "react";
+import "./style.css"
+
+const wordMeaning = {
+  "Aiz": "Anything happenes say this",
+  "Modi": "Modi hei to munkin hie",
+  "Tapa Tap": "Bhau's best dialogue"
+};
+
+var wordsWeKnow = Object.keys(wordMeaning);
 
 export default function App() {
-  const [likeCounter, setLikeCounter] = useState(0);
 
-  function likeClickHandler() {
-    var newLikeCounterValue = likeCounter + 1;
-    setLikeCounter(newLikeCounterValue);
+  const [meaning, setMeaning] = useState("")
+
+  function meaningInputHandler(event){
+    var userInput = event.target.value;
+
+    var meaning = wordMeaning[userInput]
+
+    if (meaning === undefined) {
+      meaning = "Nikal"
+    }
+
+    setMeaning(meaning)
   }
 
+  function wordClickHandler(word){
+    var meaning = wordMeaning[word];
+    setMeaning(meaning) 
+  }
+  
   return (
     <div>
       <h1>Inside Out</h1>
-      <button onClick={likeClickHandler}>Like Me!</button> {likeCounter}
+     <input onChange={meaningInputHandler} type="text" />
+
+     <h2>Meaning: {meaning}</h2>
+     
+     <h3>Words we know</h3>
+     {
+       wordsWeKnow.map(function(word) {
+         return <span 
+         onClick={()=> wordClickHandler(word)} 
+
+          key={word} 
+
+          style={{margin: "30px", cursor: "pointer"}}>{word}</span>
+       })
+     }
     </div>
   );
 }
