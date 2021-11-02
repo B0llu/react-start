1120ccbce1c773a0373471c027427bb6c1cf2777
Dhruv <samantdhruv@gmail.com>
2021-11-02 12:22:41 +0530

    styling, better use of variables, using jsx

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 function BookList() {
   return (
-    <section>
+    <section className="booklist">
       <Book />
     </section>
   );
 }
 
+const author = "Morgan Housel"; //using outside still works
 const Book = () => {
+  const title = "The Psychology of Money"; //can be used inside of function as well as outside
   return (
-    <article>
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg"
+        alt=""
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   );
 };
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg"
-    alt=""
-  />
-);
-const Title = () => <h1>The Psychology of Money</h1>;
-const Author = () => <h4>Morgan Housel</h4>;
 
 ReactDOM.render(BookList(), document.getElementById("root"));
