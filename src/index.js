3dc13345c484d8e9c41aa9870735faf0892fd1ec
Dhruv <samantdhruv@gmail.com>
2021-11-02 11:23:43 +0530

    small project using variable components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  )
+    <section>
+      <Book />
+    </section>
+  );
 }
 
-const Person = () => <h2>Dhruv Samant</h2>
-const Message = () => {
-  return <p>Hi whats up man?</p>
-}
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg"
+    alt=""
+  />
+);
+const Title = () => <h1>The Psychology of Money</h1>;
+const Author = () => <h4>Morgan Housel</h4>;
 
-ReactDOM.render(Greeting(), document.getElementById('root'));
+ReactDOM.render(BookList(), document.getElementById("root"));
