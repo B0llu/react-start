9bed9aefc5702af9894dbf67dbdd397b5da9f618
Dhruv <samantdhruv@gmail.com>
2021-11-03 09:26:04 +0530

    using props to show 2 different books

--- src/index.js
+++ src/index.js
@@ -2,31 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg",
+  title: "The Psychology of Money",
+  author: "Morgan Housel",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/P/B01N5AX61W.01._SCLZZZZZZZ_SX500_.jpg",
+  title: "Atomic Habits",
+  author: "James Clear",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book name="Dhruv" />
-      <Book surname="samant" />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
 const Book = (props) => {
-  const firstBook = {
-    img: "https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg",
-    title: "The Psychology of Money",
-    author: "Morgan Housel"
-  }
-  //the const can be used inside of function as well as outside, they work in both conditions
-  const title = "The Psychology of Money";
-  const author = "Morgan Housel";
-  const img = "https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg"
-    
   return (
     <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
