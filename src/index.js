0235c7617310a579c05143c81d33d25550166eca
Dhruv <samantdhruv@gmail.com>
2021-11-03 09:00:39 +0530

    learning props

--- src/index.js
+++ src/index.js
@@ -5,22 +5,28 @@ import "./index.css";
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book name="Dhruv" />
+      <Book surname="samant" />
     </section>
   );
 }
 
-const author = "Morgan Housel"; //using outside still works
-const Book = () => {
-  const title = "The Psychology of Money"; //can be used inside of function as well as outside
+const Book = (props) => {
+  const firstBook = {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg",
+    title: "The Psychology of Money",
+    author: "Morgan Housel"
+  }
+  //the const can be used inside of function as well as outside, they work in both conditions
+  const title = "The Psychology of Money";
+  const author = "Morgan Housel";
+  const img = "https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg"
+    
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg"
-        alt=""
-      />
+      <img src={img} alt="" />
       <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <h4>{author}</h4>
     </article>
   );
 };
