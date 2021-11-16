ad96759340cfb3e345f4ef3c57abec63c10c089d
Dhruv <samantdhruv@gmail.com>
2021-11-16 09:54:38 +0530

    useStateArray

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // first way
+  // const [person, setPerson] = useState({
+  //   name: "Dhruv",
+  //   age: 20,
+  //   message: "Tapa Tap",
+  // });
+
+  //second way
+  const [name, setName] = useState("Dhruv");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("Tapa Tap");
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: "Sochna padta he re konsi gali du" });
+    setMessage("Sochna padta he re konsi gali du");
+  };
+  return (
+    <>
+      <h3>{name}</h3> 
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
