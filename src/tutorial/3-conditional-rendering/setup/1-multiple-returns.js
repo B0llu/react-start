f3225a80a978797314f819a4dfa71101f2e3d267
Dhruv <samantdhruv@gmail.com>
2021-11-17 11:06:46 +0530

    calling a api to set a useState logic with if else statements

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,12 +1,49 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
+
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(false)
-  if (loading) {
-    return <h2>Loading....</h2>
+  const [isloading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUsers] = useState("default user");
+
+  useEffect(() => {
+    fetch(url)
+      .then((Response) => {
+        if (Response.status >= 200 && Response.status <= 299) {
+          return Response.json();
+        } else {
+          setIsLoading(false)
+          setIsError(true)
+          throw new Error (Response.statusText)
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUsers(login);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isloading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else if (isError) {
+    return (
+      <div>
+        <h1>Error.....</h1>
+      </div>
+    );
   } else {
-    return <h2>Multiple Returns</h2>
-  } 
+    return (
+      <div>
+        <h1>{user}</h1>
+      </div>
+    );
+  }
 };
 
 export default MultipleReturns;
