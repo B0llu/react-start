1aef172baa2bdca377e1c3f1ae7635590328411f
Dhruv <samantdhruv@gmail.com>
2021-11-17 11:44:20 +0530

    using short circuit ( AND OR) to handle errors

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h1>{text || "Dhruv"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <h2>There is an Error....</h2>
+      ) : (
+        <div>
+          <h2>There is no Error....</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
